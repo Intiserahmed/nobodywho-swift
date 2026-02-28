dbe52b73b9ba029308a9e7ee930597aa1829564e
Intiser Ahmed <57252940+Intiserahmed@users.noreply.github.com>
2026-02-28 21:43:32 +0600

    Update binary target URL to point to main repo releases

--- Package.swift
+++ Package.swift
@@ -24,11 +24,11 @@ let package = Package(
             dependencies: ["NobodyWhoFFI"],
             path: "Sources/NobodyWho"
         ),
-        // XCFramework distributed via GitHub releases
+        // XCFramework distributed via GitHub releases from the main nobodywho repo
         .binaryTarget(
             name: "NobodyWhoFFI",
-            url: "https://github.com/nobodywho-ooo/nobodywho/releases/download/nobodywho-swift-v0.1.0/NobodyWhoFFI.xcframework.zip",
-            checksum: "120b7e51aef498ae958d32a7adb79ab02839e8e8bf3963a0382af1b2b7138626"
+            url: "https://github.com/Intiserahmed/nobodywho/releases/download/v0.1.0/NobodyWhoFFI.xcframework.zip",
+            checksum: "51d0103b8d63c9360bfb2d2d08017bd19eac45fc12de7fa29e68582f4366efb3"
         ),
         .executableTarget(
             name: "NobodyWhoTestCLI",
